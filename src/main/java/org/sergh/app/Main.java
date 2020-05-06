52045e91d3a4486fad05477c8a5296c7b8f8a169
Sergey Mezinov <s3rgh@mail.ru>
2020-05-06 10:06:07 +0300

    06-05-2020 modify README.md and some fixes

--- src/main/java/org/sergh/app/Main.java
+++ src/main/java/org/sergh/app/Main.java
@@ -2,14 +2,12 @@ package org.sergh.app;
 
 import org.sergh.app.execution.ExecutorCallsService;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
     public static void main(String[] args) {
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        new ExecutorCallsService().parallelExecutor();
+
+        /*BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         try {
             System.out.println("Enter \"1\", if you want manually create call or - \"2\" if you want to start autocreating 1000 records.");
             System.out.println("Enter \"exit\" for close program.");
@@ -32,6 +30,6 @@ public class Main {
             bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
